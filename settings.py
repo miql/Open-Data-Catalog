6f46ccf1762b0be270d5538146f75ad5f6e25352
Aaron Kavlie <akavlie@gmail.com>
2011-08-17 11:56:01 -0700

    Corrected path for opendata static files.

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/projects/OpenDataCatalog/opendata/static",
+    os.path.join(os.path.dirname(__file__), 'opendata/static'),
 )
 
 # List of finder classes that know how to find static files in
